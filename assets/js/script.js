135a9efc973fa326a3d2d603d64308e7d0fcfe57
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-01-18 15:27:49 -0500

    Quiz working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,193 +1,105 @@
-//var highScores = document.querySelector(".highscores");
-//var timeCounter = 0;
-
-// Highscore Function - move to end?
-//highScores.addEventListener("click", function(event) {
-//    console.log(event);
-//  var div2 = doucment.createElement("div");
-//  div2.textContent
-//});
-
+// Setup localStorage for highscores and initials they will put in
+var highScores = document.querySelector("#highscores");
+var currentScore = 0;
 var timeLeft = 10;
-var quizQuestions = document.querySelector(".quiz-qa");
-// do i need a blank variable for the answers they will provide var studentAnswers = [];
-debugger;
-console.log(quizOne);
+var quizQuestions = document.querySelector("#quiz-qa");
+var question = document.querySelector("#question");
+var option1 = document.querySelector("#option1");
+var option2 = document.querySelector("#option2");
+var option3 = document.querySelector("#option3");
+var option4 = document.querySelector("#option4");
 
-// Listen for a click in order to proceed with the startQuiz
-quizQuestions.addEventListener("click", function() {
-    console.log(quizQuestions);
-    // Timer that will countdown from 10
-    var startQuiz = document.querySelector(".start-quiz");
+//quizQuestions.setInerval( {
 
-    for (var i = 0; i < quizOne.length; i++) {
-        //Create questions and answers
-        var div = document.createElement("div");
-        // Quiz question
-        var h2 = document.createElement('h2');
-        // Quiz answer options
-        var ul = document.createElement('ul');
-        var li = doucment.createElement('li');
-        var li1 = document.createElement('li');
-        var li2 = doucment.createElement('li');
-        var li3 = doucment.createElement('li');
+console.log(quizQuestions);
 
+var startQuiz = document.querySelector("#start-quiz");
+    var currentQuestion = 0;
+    console.log(startQuiz);
 
-        // Use the 'setInterval()' method to call 'startQuiz()' function to be executed per question
-       var startQuiz = setInterval(function() {
-           //If you click start then start the test
-           // Should I be using .push instead of if() {}
-            if (quizQuestions === "clicked") {
-                // Clear textContent first
-                quizQuestions.textContent = "";
+startQuiz.addEventListener("click", function() {
+    console.log(quizOne[currentQuestion]);
 
-                // Add content to li's created
-                li.textContent = quiz[i].option1;
-                li1.textContent = quiz[i].option2;
-                li2.textContent = quiz[i].option3;
-                li3.textContent = quiz[i].option4;
+    question.innerText = quizOne[currentQuestion].question;
+        option1.innerText = quizOne[currentQuestion].option1;
+        option2.innerText = quizOne[currentQuestion].option2;
+        option3.innerText = quizOne[currentQuestion].option3;
+        option4.innerText = quizOne[currentQuestion].option4;
+    });
 
-                // Add elements and their content to ul
-                ul.appendChild(li);
-                ul.appendChild(li1);
-                ul.appendChild(li2);
-                ul.appendChild(li3);
-
-                ul.appendChild(div);
-
-                // Add content to div created
-                h2.textContent = quiz[i].question1;
-
-                // Add h2 and it's content to div
-                div.appendChild(h2);
-
-                // Add div to HTML
-                quizQuestions.appendChild(div);
-
-                // Subtract timeLeft by 1
-                timeleft--;
-            }
-            // Need to make this not based on time but if previous answer is right
-            // and one big else at the end to show for wrong answers
-            else if (timeleft > 8) {
-                quizQuestions.textContent = "";
-
-                li.textContent = quiz[i].option5;
-                li1.textContent = quiz[i].option6;
-                li2.textContent = quiz[i].option7;
-                li3.textContent = quiz[i].option8;
-
-                ul.appendChild(li);
-                ul.appendChild(li1);
-                ul.appendChild(li2);
-                ul.appendChild(li3);
-                
-                ul.appendChild(div);
-                
-                h2.textContent = quiz[i].question2;
-                
-                div.appendChild(h2);
-                
-                quizQuestions.appendChild(div);
-
-                timeleft--;
-            }
-            // Need to make this not based on time but if previous answer is right
-            else if (timeleft > 6) {
-                quizQuestions.textContent = "";
-
-                li.textContent = quiz[i].option9;
-                li1.textContent = quiz[i].option10;
-                li2.textContent = quiz[i].option11;
-                li3.textContent = quiz[i].option12;
-
-                ul.appendChild(li);
-                ul.appendChild(li1);
-                ul.appendChild(li2);
-                ul.appendChild(li3);
-                
-                ul.appendChild(div);
-                
-                h2.textContent = quiz[i].question3;
-                
-                div.appendChild(h2);
-                
-                quizQuestions.appendChild(div);
-
-                timeleft--;
-            }
-            // Need to make this not based on time but if previous answer is right
-            else if (timeleft > 4) {
-                quizQuestions.textContent = "";
-
-                li.textContent = quiz[i].option13;
-                li1.textContent = quiz[i].option14;
-                li2.textContent = quiz[i].option15;
-                li3.textContent = quiz[i].option16;
-
-                ul.appendChild(li);
-                ul.appendChild(li1);
-                ul.appendChild(li2);
-                ul.appendChild(li3);
-                
-                ul.appendChild(div);
-                
-                h2.textContent = quiz[i].question4;
-                
-                div.appendChild(h2);
-                
-                quizQuestions.appendChild(div);
-
-                timeleft--;
-            }
-            // Need to make this not based on time but if previous answer is right
-            else if (timeleft > 2) {
-                quizQuestions.textContent = "";
-
-                li.textContent = quiz[i].option17;
-                li1.textContent = quiz[i].option18;
-                li2.textContent = quiz[i].option19;
-                li3.textContent = quiz[i].option20;
-
-                ul.appendChild(li);
-                ul.appendChild(li1);
-                ul.appendChild(li2);
-                ul.appendChild(li3);
-                
-                ul.appendChild(div);
-                
-                h2.textContent = quiz[i].question5;
-                
-                div.appendChild(h2);
-                
-                quizQuestions.appendChild(div);
-
-                timeleft--;
-            }
-            else {
-                quizQuestions.textContent = "";
-
-                // Use clearInterval() to stop the timer
-                clearInterval(startQuiz);
-                
-                // Call the finalMessage() function
-                finalMessage();
-            }
-        }, 10000 );
+option1.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log(event.target.innerText);
+    console.log(quizOne[currentQuestion].answer);
+    if (event.target.innerText === quizOne[currentQuestion].answer) {
+        currentScore++;
+    } else {
+        timeLeft = timeLeft - 5;
     }
+
+    currentQuestion++;
+
+    question.innerText = quizOne[currentQuestion].question;
+    option1.innerText = quizOne[currentQuestion].option1;
+    option2.innerText = quizOne[currentQuestion].option2;
+    option3.innerText = quizOne[currentQuestion].option3;
+    option4.innerText = quizOne[currentQuestion].option4;
 });
 
-var finalMessage = function() {
-    var displayMessage = setInterval(function() {
-    // If there are no more questions
-    if (timeLeft === undefined) {
-        // Use clearInterval to stop timer
-        clearInterval(displayMessage);
+option2.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log(event.target.innerText);
+    console.log(quizOne[currentQuestion].answer);
+    if (event.target.innerText === quizOne[currentQuestion].answer) {
+        currentScore++;
     } else {
-        // Need to display 'All done' page with the ability to enter initials
-        quizQuestions.textContent = "All done page";
+        timeLeft = timeLeft - 5;
     }
-}, 10000);
-}
 
-quizQuestions();
+    currentQuestion++;
+
+    question.innerText = quizOne[currentQuestion].question;
+    option1.innerText = quizOne[currentQuestion].option1;
+    option2.innerText = quizOne[currentQuestion].option2;
+    option3.innerText = quizOne[currentQuestion].option3;
+    option4.innerText = quizOne[currentQuestion].option4;
+});
+
+option3.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log(event.target.innerText);
+    console.log(quizOne[currentQuestion].answer);
+    if (event.target.innerText === quizOne[currentQuestion].answer) {
+        currentScore++;
+    } else {
+        timeLeft = timeLeft - 5;
+    }
+
+    currentQuestion++;
+
+    question.innerText = quizOne[currentQuestion].question;
+    option1.innerText = quizOne[currentQuestion].option1;
+    option2.innerText = quizOne[currentQuestion].option2;
+    option3.innerText = quizOne[currentQuestion].option3;
+    option4.innerText = quizOne[currentQuestion].option4;
+});
+
+option4.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log(event.target.innerText);
+    console.log(quizOne[currentQuestion].answer);
+    if (event.target.innerText === quizOne[currentQuestion].answer) {
+        currentScore++;
+    } else {
+        timeLeft = timeLeft - 5;
+    }
+    console.log(currentScore);
+
+    currentQuestion++;
+
+    question.innerText = quizOne[currentQuestion].question;
+    option1.innerText = quizOne[currentQuestion].option1;
+    option2.innerText = quizOne[currentQuestion].option2;
+    option3.innerText = quizOne[currentQuestion].option3;
+    option4.innerText = quizOne[currentQuestion].option4;
+});
--- assets/quiz.js
+++ assets/quiz.js
@@ -1,36 +1,45 @@
-// Quiz 1 - answer = 
-var quizOne = {
-    question1: "Commonly used data types DO NOT include:",
+// Quiz 1 - answer = 3. alerts
+var quizOne = [{ 
+    question: "Commonly used data types DO NOT include:",
     option1: "1. strings",
     option2: "2. booleans",
     option3: "3. alerts",
     option4: "4. numbers",
-
+    answer: " 3. alerts"
+},
 // Quiz 2 - answer = 
-    question2: "The condition in an if/else statement is enclosed with ____.",
-    option5: "1. quotes",
-    option6: "2. curly brackets",
-    option7: "3. parentheses",
-    option8: "4. square brackets",
-
+{
+    question: "The condition in an if/else statement is enclosed with ____.",
+    option1: "1. quotes",
+    option2: "2. curly brackets",
+    option3: "3. parentheses",
+    option4: "4. square brackets",
+    answer: "2. curly brackets"
+},
 // Quiz 3 - answer = 3. booleans
-    question3: "Arrays in JavaScript can be used to store ____.",
-    option9: "1. numbers and strings",
-    option10: "2. other arrays",
-    option11: "3. booleans",
-    option12: "4. all of the above",
-
+{
+    question: "Arrays in JavaScript can be used to store ____.",
+    option1: "1. numbers and strings",
+    option2: "2. other arrays",
+    option3: "3. booleans",
+    option4: "4. all of the above",
+    answer: "3. booleans"
+},
 // Quiz 4 - answer = 4. parentheses
-    question4: "String values must be enclosed within ____ when being assigned to variables.",
-    option13: "1. commas",
-    option14: "2. curly brackets",
-    option15: "3. quotes",
-    option16: "4. parentheses",
-
+{
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    option1: "1. commas",
+    option2: "2. curly brackets",
+    option3: "3. quotes",
+    option4: "4. parentheses",
+    answer: "4. parentheses"
+},
 // Quiz 5 - answer = 4. console.log
-    question5: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    option17: "1. JavaScript",
-    option18: "2. terminal/bash",
-    option19: "3. for loops",
-    option20: "4. console.log"
-}
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    option1: "1. JavaScript",
+    option2: "2. terminal/bash",
+    option3: "3. for loops",
+    option4: "4. console.log",
+    answer: "4. console.log"
+}]
