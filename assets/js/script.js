683ca015ce681628e91a4d9a9ceb7007f90d24cb
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-01-21 12:23:24 -0500

    add timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,47 +2,54 @@
 var highScores = document.querySelector("#highscores");
 var currentScore = 0;
 var timeCounter = document.querySelector("#time-counter");
-var timeLeft = 10;
+var timeLeft = 100;
 var quizQuestions = document.querySelector("#quiz-qa");
 var question = document.querySelector("#question");
 var option1 = document.querySelector("#option1");
 var option2 = document.querySelector("#option2");
 var option3 = document.querySelector("#option3");
 var option4 = document.querySelector("#option4");
+var secondsEl = document.querySelector("#seconds");
 
 console.log(quizQuestions);
 
-// Timer
-var countdown = setInterval(function(timeLeft) {
-    console.log(timeLeft);
+var showTime = function() {
+    secondsEl.textContent = timeLeft;
 
-    if (timeLeft > 10) {
-        timeCounter.textContent = timeLeft + ' minutes';
-        timeLeft--;
-    } else if (timeLeft === 1) {
-        timeCounter.textContent = timeLeft + ' seconds';
-    } else {
-        timeCounter.textContent = "";
-        clearInterval(countdown);
-    }
-
-    timeLeft.appendChild(timeCounter);
-
-}, 1000);
+    var countdown = setInterval(function() {
+        console.log(timeLeft);
+    
+        if (timeLeft < 100) {
+            document.getElementById("quiz-qa").style.display = "none";
+        } else if (timeLeft > 1) {
+            secondsEl.textContent = timeLeft;
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            secondsEl.textContent = timeLeft;
+        } else {
+            clearInterval(countdown);
+        }
+    
+    }, 1000);
+};
 
 // Begin the test
 var startQuiz = document.querySelector("#start-quiz");
     var currentQuestion = 0;
     console.log(startQuiz);
 
+
 startQuiz.addEventListener("click", function() {
     console.log(quizOne[currentQuestion]);
+
+    showTime();
 
     question.innerText = quizOne[currentQuestion].question;
         option1.innerText = quizOne[currentQuestion].option1;
         option2.innerText = quizOne[currentQuestion].option2;
         option3.innerText = quizOne[currentQuestion].option3;
         option4.innerText = quizOne[currentQuestion].option4;
+
     });
 
 option1.addEventListener("click", function(event) {
@@ -62,6 +69,9 @@ option1.addEventListener("click", function(event) {
     option2.innerText = quizOne[currentQuestion].option2;
     option3.innerText = quizOne[currentQuestion].option3;
     option4.innerText = quizOne[currentQuestion].option4;
+
+    //Css 
+    document.getElementsByTagName("button")[0].setAttribute("class", "start-test");
 });
 
 option2.addEventListener("click", function(event) {
